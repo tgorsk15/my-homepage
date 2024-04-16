76b228c46289bd239dfb35afd2737ad50b2bf150
Tyler Gorski <115673766+tgorsk15@users.noreply.github.com>
2024-04-16 16:11:36 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Why are we still here?");
+console.log("Yes");
+function templateStarter() {
+  console.log("is Prettier working?");
+}
+function newList() {
+  console.log("yay");
+}
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyx3QkFBd0IsQ0FBQztBQUNyQ0QsT0FBTyxDQUFDQyxHQUFHLENBQUMsS0FBSyxDQUFDO0FBRWxCLFNBQVNDLGVBQWVBLENBQUEsRUFBRztFQUN6QkYsT0FBTyxDQUFDQyxHQUFHLENBQUMsc0JBQXNCLENBQUM7QUFDckM7QUFFQSxTQUFTRSxPQUFPQSxDQUFBLEVBQUc7RUFDakJILE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLEtBQUssQ0FBQztBQUNwQixDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWFpbi10ZW1wbGF0ZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIldoeSBhcmUgd2Ugc3RpbGwgaGVyZT9cIik7XG5jb25zb2xlLmxvZyhcIlllc1wiKTtcblxuZnVuY3Rpb24gdGVtcGxhdGVTdGFydGVyKCkge1xuICBjb25zb2xlLmxvZyhcImlzIFByZXR0aWVyIHdvcmtpbmc/XCIpO1xufVxuXG5mdW5jdGlvbiBuZXdMaXN0KCkge1xuICBjb25zb2xlLmxvZyhcInlheVwiKTtcbn1cbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIiwidGVtcGxhdGVTdGFydGVyIiwibmV3TGlzdCJdLCJzb3VyY2VSb290IjoiIn0=
